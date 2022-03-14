3cd5098063d11ea78fdca6967a5351eb2eda330c
Alex Ulloa <alex.ulloa@thoughtworks.com>
2022-03-14 17:00:34 -0500

    Contenido SwiftUI

---
+++ swiftUI-app/swiftUI-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUI-app
+//
+//  Created by LAalex.ulloa on 14/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Lista de tareas")
+                .font(.title)
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
