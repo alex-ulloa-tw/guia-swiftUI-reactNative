087d241177800330c29960cb65d3a91f9670aa7d
Alex Ulloa <alex.ulloa@thoughtworks.com>
2022-03-15 15:51:22 -0500

    State

--- swiftUI-app/swiftUI-app/ContentView.swift
+++ swiftUI-app/swiftUI-app/ContentView.swift
@@ -8,10 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showExplanation = true
     var body: some View {
         VStack {
             Text("Lista de tareas")
                 .font(.title)
+            
+            if showExplanation {
+                Text("Aquí tienes tu lista de tares pendientes")
+                    .padding(.vertical)
+            }
+            
+            Button("\(showExplanation ? " Ocultar" : "Mostrar") instrucciones") {
+                showExplanation.toggle()
+            }
+            .padding()
+            
             Spacer()
         }
     }
